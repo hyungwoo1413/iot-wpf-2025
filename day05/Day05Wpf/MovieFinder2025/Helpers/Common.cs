4e67fc0a1bc66023ffd9dc77b2667441279690b0
hyungwoo1413 <glhwguddn@gmail.com>
2025-05-14 16:48:18 +0900

    2025-05-14

---
+++ day05/Day05Wpf/MovieFinder2025/Helpers/Common.cs
@@ -0,0 +1,16 @@
+﻿using MahApps.Metro.Controls.Dialogs;
+using NLog;
+
+namespace MovieFinder2025.Helpers
+{
+    internal class Common
+    {
+        // NLog 인스턴스
+        public static readonly Logger LOGGER = LogManager.GetCurrentClassLogger();
+
+        // DB연결문자열을 한군데 저장
+        public static readonly string CONNSTR = "Server=localhost;Database=moviefinder;Uid=root;Pwd=12345;Charset=utf8";
+
+        public static IDialogCoordinator DIALOGCOORDINATOR;
+    }
+}
